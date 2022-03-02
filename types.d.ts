df90b503b78fc2b6a6cf8ef4dc1699251bc95d80
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-03-02 18:02:33 -0500

    Initial commit

---
+++ types.d.ts
@@ -0,0 +1,24 @@
+import {BeDecoratedProps} from 'be-decorated/types';
+
+export interface BeLazyVirtualProps{
+    options: IntersectionObserverInit;
+    isIntersecting: boolean;
+    isIntersectingEcho: boolean;
+    enterDelay: number;
+}
+
+export interface BeLazyProps extends BeLazyVirtualProps{
+    proxy: HTMLTemplateElement & BeLazyVirtualProps;
+}
+
+export interface BeLazyActions{
+
+    intro(proxy: HTMLTemplateElement & BeLazyProps, target: HTMLTemplateElement, beDecorProps: BeDecoratedProps): void;
+
+    onOptions(self: this): void;
+
+    onIntersecting(self: this): void;
+
+    finale(proxy: HTMLTemplateElement & BeLazyProps, target: HTMLTemplateElement, beDecorProps: BeDecoratedProps): void;
+
+}
