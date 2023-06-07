f779dcaf0c51567d76b5e0ee6b47619920ba3b12
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-06-06 21:02:42 -0400

    0.0.27

--- types.d.ts
+++ types.d.ts
@@ -1,26 +1,25 @@
-import {BeDecoratedProps, MinimalProxy} from 'be-decorated/types';
-import { RenderContext, MatchRHS } from '../trans-render/lib/types';
 import { 
     EndUserProps as BeIntersectiontalEndUserProps, 
-    VirtualProps as BeIntersectionalVirtualProps,
+    AllProps as BeIntersectionalAllProps,
     Actions as BeIntersectionalActions,
 } from 'be-intersectional/types';
 
 
 export interface EndUserProps extends BeIntersectiontalEndUserProps{
-    transform?: {[key: string]: MatchRHS};
-    host: any;
-    ctx: RenderContext;
+    //transform?: {[key: string]: MatchRHS};
+    //host: any;
+    //ctx: RenderContext;
 }
 
-export interface VirtualProps extends BeIntersectionalVirtualProps, EndUserProps{}
+export interface AllProps extends BeIntersectionalAllProps, EndUserProps{}
 
-export type Proxy = HTMLTemplateElement & VirtualProps;
+export type AP = AllProps;
 
-export interface ProxyProps extends VirtualProps{
-    proxy: Proxy;
-}
+export type PAP = Partial<AP>;
 
-export type PP = ProxyProps;
+export type ProPAP = Promise<PAP>;
+
+//export type POA = [PAP | undefined, ActionOnEventConfigs<PAP, Actions>];
+
 
 export interface Actions extends BeIntersectionalActions{}
